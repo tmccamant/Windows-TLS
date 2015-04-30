bca1925839b43869e5c51d8f44448952e1bc7018
tmccamant <tmccamant@gmail.com>
2015-04-30 14:40:25 -0400

    Create Default-SSL.ps1

---
+++ Default-SSL.ps1
@@ -0,0 +1,13 @@
+# Created by: Tyler McCamant (tmccamant@gmail.com)
+#
+# This will reset the default SSL/TLS settings for Windows
+
+# Reset default settings
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers' -Force | Out-Null
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Hashes' -Force | Out-Null
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\KeyExchangeAlgorithms' -Force | Out-Null
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols' -Force | Out-Null
+
+# Disable SSLv2 client support (Default settings for Windows)
+New-Item 'HKLM:SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name DisabledByDefault -value 1 -PropertyType 'DWord' -Force | Out-Null
